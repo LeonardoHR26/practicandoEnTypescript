cbb02e9275d4483d3729dfdd040704206c738b49
LeonardoHR26 <93102919+LeonardoHR26@users.noreply.github.com>
2024-09-29 13:10:58 -0400

    Add files via upload

--- cuenta.js
+++ cuenta.js
@@ -1,17 +1,30 @@
 "use strict";
+let intentos = 0;
 const validadorCuenta = () => {
     const userInput = document.getElementById('user');
     const passwordInput = document.getElementById('password');
-    const accountOn = document.getElementById(`account`);
+    const accountOn = document.getElementById('account');
+    const intentosFallidosInput = document.getElementById('intentosFallidos');
+    const botonLogin = document.getElementById('login');
     const inputUser = userInput.value;
     const inputPassword = passwordInput.value;
     const realUser = 'leo';
     const realpassword = 'leo';
     if (inputUser === realUser && inputPassword === realpassword) {
         accountOn.innerText = `Entraste a la cuenta usuario: ${realUser}`;
+        accountOn.style.color = `green`;
+        alert('Entraste a la cuenta, UwU');
     }
     else {
+        intentos++;
+        accountOn.style.color = `red`;
         accountOn.innerText = 'Usuario o contraseña incorrecta, intente denuevo.';
+        intentosFallidosInput.textContent = `Intentos Fallidos: ${intentos}`;
     }
-    //console.log(`${inputUser} ${inputPassword}`);
+    console.log(`Entrada usuario: ${inputUser} Entrada password: ${inputPassword}`);
+    if (intentos >= 5) {
+        alert('Numeros de intentos Maximos permitidos');
+        botonLogin.style.backgroundColor = 'red';
+        botonLogin.disabled;
+    }
 };
--- cuenta.ts
+++ cuenta.ts
@@ -1,9 +1,13 @@
+let intentos : number = 0;
 
 const validadorCuenta = () => {
 
     const userInput = document.getElementById('user') as HTMLInputElement;
     const passwordInput = document.getElementById('password') as HTMLInputElement;
-    const accountOn = document.getElementById(`account`) as HTMLElement;
+    const accountOn = document.getElementById('account') as HTMLElement;
+    const intentosFallidosInput = document.getElementById('intentosFallidos') as HTMLElement;
+    const botonLogin = document.getElementById('login') as HTMLButtonElement;
+
 
     const inputUser : string = userInput.value;
     const inputPassword : string = passwordInput.value;
@@ -12,19 +16,40 @@ const validadorCuenta = () => {
     const realpassword = 'leo';
 
     if (inputUser === realUser && inputPassword === realpassword){
-        accountOn.innerText = `Entraste a la cuenta usuario: ${realUser}` 
+        accountOn.innerText = `Entraste a la cuenta usuario: ${realUser}`;
+        accountOn.style.color = `green`;
+        alert('Entraste a la cuenta, UwU');
     }
 
     else {
+        intentos++;
+        accountOn.style.color = `red`;
         accountOn.innerText = 'Usuario o contraseña incorrecta, intente denuevo.'
+        intentosFallidosInput.textContent= `Intentos Fallidos: ${intentos}`;
     }
     
+    console.log(`Entrada usuario: ${inputUser} Entrada password: ${inputPassword}`);
+
+    if (intentos >= 5){
+
+        alert('Numeros de intentos Maximos permitidos');
+
+        botonLogin.style.backgroundColor = 'red';
+        botonLogin.disabled;
+
+    }
 
 
-    //console.log(`${inputUser} ${inputPassword}`);
+    }
+
+    
 
     
 
 
 
-}
+
+
+
+
+
